3f97e04c920f5292a88ddc7781d8c416bd525507
Nikhil M Jain <nikhilporwal94@gmail.com>
2018-03-28 13:01:32 +0530

    createbst

---
+++ bstcreate.py
@@ -0,0 +1,42 @@
+l = [3,6,1,2,5,4]
+class Node():
+    def __init__(self, info = None):
+        self.info = info
+        self.l = self.r = None
+s = []
+def inorder(head):
+    
+    cur = head
+    while s != [] or cur:
+
+        while cur:
+            s.append(cur)
+            cur = cur.l
+        
+        cur = s.pop()
+        print cur.info
+
+        cur = cur.r
+
+
+def insertbst(root):
+    root = Node(l[0])
+    for i in l[1:]:
+        p = Node(i)
+        q = root
+        while True:
+            if p.info < q.info:
+                if not q.l:
+                    q.l = p
+                    break
+                else:
+                    q = q.l
+            else:
+                if not q.r:
+                    q.r = p
+                    break
+                else:
+                    q = q.r
+    inorder(root)
+
+insertbst(None)
