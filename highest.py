e1a5d967df81140c4e17df71920c2f67025d123f
Nikhil M Jain <nikhilporwal94@gmail.com>
2018-04-21 17:13:36 +0530

    update

--- highest.py
+++ highest.py
@@ -1,6 +1,6 @@
 #Given an array in such a way that the elements stored in array are in increasing order initially and then after reaching to a peak element , elements stored are in decreasing order. Find the highest element.
 
-a = [1,2,3,4,5,6,7,8,9]
+a = [1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1]
 
 l = 0 
 r = len(a) - 1
@@ -11,4 +11,4 @@ while(l < r):
     else:
         r -= 1
 
-print a[l]
+print (a[l])
