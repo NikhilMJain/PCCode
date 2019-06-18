aafd5465290cffe5185e3292bb7df0bfd2cb42a1
Nikhil M Jain <nikhilporwal94@gmail.com>
2019-06-18 21:24:06 +0530

    Delete bs.py

--- One/bs.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-r = requests.get('http://shityoucanafford.com/wp-json/wp/v2').json()
-soup = BeautifulSoup(r, 'html.parser')
-#print(soup.prettify)
-mydivs = soup.findAll("div", {"class": "rtitle"})
-
-print(mydivs)
